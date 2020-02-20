45297e95f5a192a479b770730f40076b0acb6ca6
Levi Butcher <levibutcher355@gmail.com>
2020-02-19 23:19:52 -0500

    Setup main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate bankers;
+use bankers::bankers_algorithm;
+
 fn main() {
+    // Parse text files into bankers algorithms required structure
+    // Could run the parse of the file and bankers algorithm in threads, return bankers result from thread,
+    // Then print the result
+
     println!("Hello, world!");
 }
